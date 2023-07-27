99672f09b9237490406ecfdbf68063847fa303b2
darrenkuro <odon5ht@gmail.com>
2023-07-27 10:24:29 +0200

    module07/ex01 init

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -6,21 +6,30 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 09:22:23 by dlu               #+#    #+#             */
-/*   Updated: 2023/07/27 09:36:07 by dlu              ###   ########.fr       */
+/*   Updated: 2023/07/27 10:23:41 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T>
-// void iter(T *arr, unsigned int length, void (*f)(T const &t)) {
-//     for (unsigned int i = 0; i < length; i++)
-//         f(arr[i]);
-// };
-void iter(T *arr, unsigned int length, void (*f)(T)) {
+/**
+ * If function is explicitly declared,
+ * i.e. void (*f)(T const &), void (*f)(T &).
+ * Using a template, complier would be able to deduce any type passed in.
+ */
+template <typename T, typename F>
+void iter(T *arr, unsigned int length, F f) {
     for (unsigned int i = 0; i < length; i++)
         f(arr[i]);
 };
+
+/* For testing purpose. */
+#include <iostream>
+
+template <typename T>
+void print(T const &x) {
+    std::cout << x << std::endl;
+}
 
 #endif // ITER_HPP
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,8 +6,19 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 09:22:09 by dlu               #+#    #+#             */
-/*   Updated: 2023/07/27 09:22:20 by dlu              ###   ########.fr       */
+/*   Updated: 2023/07/27 10:22:31 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+#include <iostream>
+
+int main(void) {
+    int tab[] = {0, 1, 2};
+    std::string tab2[] = {"hello", "world", "yep"};
+
+    iter(tab, 3, print<int>);
+    iter(tab2, 3, print<std::string>);
+
+    return 0;
+}
