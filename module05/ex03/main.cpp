f2396ac73bccd6f75fa793da1bab077f443b0d78
dlu <odon5ht@gmail.com>
2023-08-03 14:13:42 +0200

    add intern tests

--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 16:26:27 by dlu               #+#    #+#             */
-/*   Updated: 2023/07/27 07:38:51 by dlu              ###   ########.fr       */
+/*   Updated: 2023/08/03 14:13:18 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,17 @@ int main(void) {
     try {
         Intern rando;
         AForm *form;
+        AForm *form1;
+        AForm *form2;
 
         form = rando.makeForm("robotomy request", "Bender");
-        std::cout << form->getName() << std::endl;
+        form1 = rando.makeForm("presidential pardon", "Bender");
+        form2 = rando.makeForm("shrubbery creation", "Bender");
         delete form;
+        delete form1;
+        delete form2;
         form = rando.makeForm("wrong request", "Bender");
+        std::cout << "test" << std::endl;
 
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
