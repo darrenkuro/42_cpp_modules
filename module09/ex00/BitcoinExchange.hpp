9f72e6d531cb315d66e7404c263704877e97c2f1
dlu <odon5ht@gmail.com>
2023-08-24 20:01:31 +0200

    update

--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 14:43:33 by dlu               #+#    #+#             */
-/*   Updated: 2023/08/08 15:10:49 by dlu              ###   ########.fr       */
+/*   Updated: 2023/08/24 18:59:35 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define BITCOINEXCHANGE_HPP
 
 #include <iostream>
+#include <map>
 
 class BitcoinExchange {
   public:
@@ -25,6 +26,7 @@ class BitcoinExchange {
     BitcoinExchange &operator=(BitcoinExchange const &t);
 
   private:
+    std::map<std::string, float> _data;
 };
 
 #endif // BITCOINEXCHANGE_HPP
